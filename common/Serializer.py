63cb3dcf443c369b0b325a5bff0f2d6c36bce4a4
jtchilders <taylor@jtchilders.com>
2016-11-21 11:09:21 -0600

    added datetime encoder

--- common/Serializer.py
+++ common/Serializer.py
@@ -1,8 +1,10 @@
-import json,logging
+import json,logging,datetime
 logger = logging.getLogger(__name__)
 
 
-def serialize(obj,sort_keys=False,pretty_print=None):
+def serialize(obj,sort_keys=False,pretty_print=None,use_datetime=True):
+   if use_datetime:
+      return json.dumps(obj,sort_keys=sort_keys,indent=pretty_print,cls=DateTimeEncoder)
    return json.dumps(obj,sort_keys=sort_keys,indent=pretty_print)
 
 def deserialize(text):
@@ -13,3 +15,10 @@ def convert_unicode_string(unicode_string):
    if unicode_string is not None:
       return str(unicode_string)
    return None
+
+class DateTimeEncoder(json.JSONEncoder):
+     def default(self, obj):
+         if isinstance(obj, datetime.datetime):
+             return obj.strftime("%Y-%m-%d %H:%M:%S %z")
+         # Let the base class default method raise the TypeError
+         return json.JSONEncoder.default(self, obj)
