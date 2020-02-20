a56bf5848384f49b4a886e6f92908a5fcd1d4c6c
Kyle Gerard Felker <felker@anl.gov>
2020-02-20 11:47:39 -0600

    Use Pascal case for MPICommand classes
    Should probably change host_type, setup_*() functions in worker.py and
    JobEnvironment.py

--- balsam/launcher/mpi_commands.py
+++ balsam/launcher/mpi_commands.py
@@ -50,7 +50,7 @@ class MPICommand(object):
         return result
 
 
-class OPENMPICommand(MPICommand):
+class OpenMPICommand(MPICommand):
     '''Single node OpenMPI: ppn == num_ranks'''
     def __init__(self):
         self.mpi = 'mpirun'
@@ -103,7 +103,7 @@ class BGQMPICommand(MPICommand):
         return f"--shape {shape} --block {block} --corner {corner} "
 
 
-class THETAMPICommand(MPICommand):
+class ThetaMPICommand(MPICommand):
     def __init__(self):
         # 64 independent jobs, 1 per core of a KNL node: -n64 -N64 -d1 -j1
         self.mpi = 'aprun'
@@ -144,7 +144,7 @@ class MPICHCommand(MPICommand):
         return ""
 
 
-class COOLEYMPICommand(MPICommand):
+class CooleyMPICommand(MPICommand):
     def __init__(self):
         # 64 independent jobs, 1 per core of a KNL node: -n64 -N64 -d1 -j1
         self.mpi = 'mpirun'
@@ -161,7 +161,7 @@ class COOLEYMPICommand(MPICommand):
         return f"--hosts {','.join(str(worker.id) for worker in workers)} "
 
 
-class SLURMMPICommand(MPICommand):
+class SlurmMPICommand(MPICommand):
     def __init__(self):
         # 64 independent jobs, 1 per core of a KNL node: -n64 -N64 -d1 -j1
         self.mpi = 'srun'
