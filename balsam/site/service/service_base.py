0d2a8e700747a9f28b32fbbaa1f45cab7921260f
msalim <msalim@anl.gov>
2020-10-29 16:36:48 -0500

    ServiceBase Event was not triggering cleanly with signal handler

--- balsam/site/service/service_base.py
+++ balsam/site/service/service_base.py
@@ -1,31 +1,31 @@
 import multiprocessing
 import signal
+import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class BalsamService(multiprocessing.Process):
     def __init__(self, client, *args, service_period=1.0, **kwargs):
         super().__init__(*args, **kwargs)
         self.client = client
-        self._exit_event = multiprocessing.Event()
+        self._EXIT_FLAG = False
         self._service_period = service_period
 
     def sig_handler(self, signum, stack):
-        self._exit_event.set()
+        self._EXIT_FLAG = True
 
     def run(self, *args, **kwargs):
         signal.signal(signal.SIGINT, self.sig_handler)
         signal.signal(signal.SIGTERM, self.sig_handler)
         self.client.close_session()
-        while True:
+        while not self._EXIT_FLAG:
             self.run_cycle()
-            try:
-                should_exit = self._exit_event.wait(timeout=self._service_period)
-            except multiprocessing.TimeoutError:
-                pass
-            else:
-                if should_exit:
-                    break
+            time.sleep(self._service_period)
+        logger.info(f"EXIT_FLAG: {self.__class__.__name__} Process cleaning up")
         self.cleanup()
+        logger.info(f"{self.__class__.__name__} Process exit")
 
     def run_cycle(self):
         pass
