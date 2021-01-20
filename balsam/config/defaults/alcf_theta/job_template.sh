0ebbe339dba2500c7d0931b33257cddf643aca43
Taylor Childers <taylor@jtchilders.com>
2021-01-20 16:30:36 +0000

    adding alcf_theta settings file and correcting cooley file

--- balsam/config/defaults/alcf_theta/job_template.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#COBALT -n {{ num_nodes }}
-#COBALT -t {{ wall_time_min }}
-#COBALT -A {{ project }}
-#COBALT -q {{ queue }}
-
-
-{{ balsam_bin }}/balsam launcher --{{ wf_filter }} --job-mode={{ job_mode }} --time-limit-minutes={{ wall_time_min-2 }}
