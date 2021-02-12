7af994089d40ddb9d5f48648931bc10513056779
msalim <msalim@anl.gov>
2021-02-11 20:57:28 -0600

    Make Site shutdown faster by replacing fixed sleep period with threading.Event().wait()

--- balsam/site/service/service_base.py
+++ balsam/site/service/service_base.py
@@ -1,6 +1,6 @@
 import logging
 import signal
-import time
+from threading import Event
 from typing import TYPE_CHECKING, Any
 
 from balsam.util import Process
@@ -15,19 +15,18 @@ class BalsamService(Process):
     def __init__(self, client: "RESTClient", *args: Any, service_period: float = 1.0, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.client = client
-        self._EXIT_FLAG = False
         self._service_period = service_period
+        self._exit_event = Event()
 
     def sig_handler(self, signum: Any, stack: Any) -> None:
-        self._EXIT_FLAG = True
+        self._exit_event.set()
 
     def _run(self, *args: Any, **kwargs: Any) -> None:
         signal.signal(signal.SIGINT, self.sig_handler)
         signal.signal(signal.SIGTERM, self.sig_handler)
         self.client.close_session()
-        while not self._EXIT_FLAG:
+        while not self._exit_event.wait(timeout=self._service_period):
             self.run_cycle()
-            time.sleep(self._service_period)
         logger.info(f"EXIT_FLAG: {self.__class__.__name__} Process cleaning up")
         self.cleanup()
         logger.info(f"{self.__class__.__name__} Process exit")
