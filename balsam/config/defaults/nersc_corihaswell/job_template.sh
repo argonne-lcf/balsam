f9e6a9f3884da5754ac360213af2bf6d609a4bdf
Taylor Childers <jchilders@anl.gov>
2021-02-25 07:28:30 -0800

    adding/updating nersc cori configs

--- balsam/config/defaults/nersc_corihaswell/job_template.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes={{ num_nodes }}
-#SBATCH --time={{ wall_time_min }}
-#SBATCH --qos={{ queue }}
-#SBATCH --account={{ project }}
-#SBATCH --constraint=knl
-
-
-{{ balsam_bin }}/balsam launcher --{{ wf_filter }} --job-mode={{ job_mode }} --time-limit-minutes={{ wall_time_min-2 }}
--- balsam/platform/compute_node/__init__.py
+++ balsam/platform/compute_node/__init__.py
@@ -2,6 +2,7 @@ from .alcf_thetagpu_node import ThetaGPUNode
 from .alcf_thetaknl_node import ThetaKNLNode
 from .compute_node import ComputeNode
 from .default import DefaultNode
+from .nersc_corihas_node import CoriHaswellNode
 from .summit_node import SummitNode
 
-__all__ = ["DefaultNode", "ThetaKNLNode", "SummitNode", "ThetaGPUNode", "ComputeNode"]
+__all__ = ["DefaultNode", "ThetaKNLNode", "SummitNode", "ThetaGPUNode", "CoriHaswellNode", "ComputeNode"]
