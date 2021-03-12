943e395cda0b33838119566d97309d25de3cb219
jtchilders <taylor@jtchilders.com>
2021-03-12 10:29:56 -0500

    removing env-var pass through for jsrun since it is done by default, and updating to latest interface

--- balsam/platform/app_run/summit.py
+++ balsam/platform/app_run/summit.py
@@ -1,26 +1,26 @@
-from .app_run import AppRun
+from .app_run import SubprocessAppRun
 
 
-class SummitJsrun(AppRun):
+class SummitJsrun(SubprocessAppRun):
     """
     https://docs.olcf.ornl.gov/systems/summit_user_guide.html#running-jobs
     """
 
     launch_command = "jsrun"
     cores_per_node = 42
+    ranks_per_task = 1 # this forces 1 MPI rank to 1 resource task TODO: make this settable
 
     def _build_cmdline(self) -> str:
-        # nid_str = ",".join(map(str, self.node_ids))
-        env_args = [("-E", f'{var}="{val}"') for var, val in self._envs.items()]
         args = [
+            SummitJsrun.launch_command,
             "-n",
             self.get_num_ranks(),  # number of "resource sets"
             "-a",
-            1,  # this forces 1 MPI rank to 1 resource task TODO: make this settable
+            self.ranks_per_task,
             "-c",
             int(self.cores_per_node / self._ranks_per_node),  # number of cores per resource set
             "-g",
             self._gpus_per_rank,  # gpus per resource set
-            *[arg for pair in env_args for arg in pair],
+            self._cmdline,
         ]
         return " ".join(str(arg) for arg in args)
