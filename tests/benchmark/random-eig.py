40747b0da538f7803d81578debb198e217a0904b
msalim <msalim@anl.gov>
2021-04-10 02:15:23 -0500

    add random-eig

---
+++ tests/benchmark/random-eig.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sys
+from numpy.linalg import eigh
+from pathlib import Path
+
+# result_file = Path.cwd().with_suffix(".eigs.npy").name  # used for # batch-cluster experiment naming
+result_file = "result.npy" # used by balsam eig.Eig App stage out
+input_file = sys.argv[1]
+data = np.load(input_file)
+print(data.nbytes / 1e9, "GB loaded")
+eigvals, eigvecs = eigh(data)
+np.save(result_file, eigvals)
