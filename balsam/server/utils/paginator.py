e8aefc1fc322b4e2d06f8c9b1e31994a0121f208
Darren Govoni <darren@rapidquest.ai>
2022-03-17 11:19:30 -0400

    Added comment

--- balsam/server/utils/paginator.py
+++ balsam/server/utils/paginator.py
@@ -10,6 +10,7 @@ T = TypeVar("T")
 
 
 class Paginator(Generic[T]):
+    """ Paging data class """
     def __init__(
         self,
         limit: int = APIQuery(MAX_PAGE_SIZE, le=MAX_PAGE_SIZE, description="Maximum number of items to return."),
