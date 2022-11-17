5f381bb454efe17f56262ff4057ded3bf9e76be9
Christine Simpson <csimpson@uan-0002.head.cm.americas.sgi.com>
2022-11-17 22:07:22 +0000

    Added sunspot files

--- balsam/platform/app_run/app_run.py
+++ balsam/platform/app_run/app_run.py
@@ -158,7 +158,7 @@ class SubprocessAppRun(AppRun):
         self._set_envs()
         cmdline = self._build_preamble() + self._build_cmdline()
         logger.info(f"{self.__class__.__name__} Popen: {cmdline}")
-        log_envs = ["OMP_NUM_THREADS", "OMP_PLACES"]
+        log_envs = ["OMP_NUM_THREADS", "OMP_PLACES","OMP_PROC_BIND"]
         for k in log_envs:
             if k in self._envs.keys():
                 logger.info(f"{self.__class__.__name__} envs: {k}={self._envs[k]}")
---
+++ balsam/platform/app_run/sunspot.py
@@ -0,0 +1,29 @@
+from .app_run import SubprocessAppRun
+
+
+class PolarisRun(SubprocessAppRun):
+    """
+    https://www.open-mpi.org/doc/v3.0/man1/mpiexec.1.php
+    """
+
+    def _build_cmdline(self) -> str:
+        node_ids = [h for h in self._node_spec.hostnames]
+        cpu_bind = self._launch_params.get("cpu_bind", "none")
+        nid_str = ",".join(map(str, node_ids))
+        args = [
+            "mpiexec",
+            "-np",
+            self.get_num_ranks(),
+            "-ppn",
+            self._ranks_per_node,
+            "--hosts",
+            nid_str,
+            "--cpu-bind",
+            cpu_bind,
+            "-d",
+            self._threads_per_rank,
+            "--envall",
+            "gpu_tile_compact.sh"
+            self._cmdline,
+        ]
+        return " ".join(str(arg) for arg in args)
