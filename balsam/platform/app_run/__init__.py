4de82ea969e9a5b0a804a568d6936a9d8784def6
Christine Simpson <48525133+cms21@users.noreply.github.com>
2023-11-28 20:22:48 -0600

    Update __init__.py

--- balsam/platform/app_run/__init__.py
+++ balsam/platform/app_run/__init__.py
@@ -1,11 +1,11 @@
 from .app_run import AppRun, LocalAppRun
+from .aurora import AuroraRun
 from .mpich import MPICHRun
 from .openmpi import OpenMPIRun
 from .perlmutter import PerlmutterRun
 from .polaris import PolarisRun
 from .slurm import SlurmRun
 from .summit import SummitJsrun
-from .aurora import AuroraRun
 from .theta import ThetaAprun
 from .theta_gpu import ThetaGPURun
 
