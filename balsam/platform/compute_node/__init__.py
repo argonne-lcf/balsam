f8d8123f07e009d7d57315f34a0d1cf24d492231
Christine Simpson <christine.simpson@gmail.com>
2025-01-28 17:32:36 -0600

    fix

--- balsam/platform/compute_node/__init__.py
+++ balsam/platform/compute_node/__init__.py
@@ -1,6 +1,5 @@
 from .alcf_aurora_node import AuroraNode
 from .alcf_polaris_node import PolarisNode
-from .alcf_sophia_node import SophiaNode
 from .compute_node import ComputeNode
 from .default import DefaultNode
 from .nersc_perlmutter import PerlmutterNode
